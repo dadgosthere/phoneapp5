6f05420e680546c1a1227830876fb440609e3c9d
dadgosthere <138229016+dadgosthere@users.noreply.github.com>
2024-02-20 20:35:47 +0300

    Add files via upload

---
+++ account/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+def login_request(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, "account/login.html", {"error": "username veya parola yanlış"})
+
+
+    return render(request, "account/login.html")
+
+def register_request(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        firstname = request.POST["firstname"]
+        lastname = request.POST["lastname"]
+        password = request.POST["password"]
+        repassword = request.POST["repassword"]
+
+        if password == repassword:
+            if User.objects.filter(username=username).exists():
+                return render(request, "account/register.html", {"error":"username zaten var"})
+
+            else:
+                if User.objects.filter(email=email).exists():
+                    return render(request, "account/register.html", {"error":"email zaten var"})
+                else:
+                    user = User.objects.create_user(username=username, email=email, first_name=firstname, last_name=lastname, password=password)
+                    user.save()
+                    return redirect('login')
+        else:
+            return render(request, "account/register.html", {"error":"parola eşleşmiyor"})
+
+    return render(request, "account/register.html")
+
+def logout_request(request):
+    logout(request)
+    return redirect("home")
